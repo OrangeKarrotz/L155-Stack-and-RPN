039b34a4c3914b387ff32b904774f3796aece0e5
Tom LAPTOP <21TK2301@barton.ac.uk>
2022-03-10 09:44:35 +0000

    Add project files.

---
+++ L155 Stack and RPN/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L155_Stack_and_RPN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack(5);
+            ConsoleKey input;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@"	1. Peek - Return the top item without removing it
+	2. Pop - Remove and return the top item
+	3. Push - Add a new item to the top
+	4. IsEmpty - Check if the stack is empty
+        5. IsFull - Check if the stack is full
+        [esc] - Exit
+        Debugging:
+        6. Count - num of elements in stack
+        Options:
+        7. RPN Evaluator - evaluates RPN input");
+                input = Console.ReadKey().Key;
+                Console.WriteLine();
+                switch (input)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine(myStack.Peek());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine(myStack.Pop());
+                        break;
+                    case ConsoleKey.D3:
+                        int pushing;
+                        do
+                        {
+                            Console.Write("Enter a value to push: ");
+                        } while (!int.TryParse(Console.ReadLine(), out pushing));
+                        myStack.Push(pushing);
+                        break;
+                    case ConsoleKey.D4:
+                        Console.WriteLine(myStack.IsEmpty());
+                        break;
+                    case ConsoleKey.D5:
+                        Console.WriteLine(myStack.IsFull());
+                        break;
+                    case ConsoleKey.D6:
+                        Console.WriteLine(myStack.Count());
+                        break;
+                    case ConsoleKey.D7:
+                        Evaluate();
+                        break;
+                    default:
+                        Console.WriteLine("Please provide a valid input");
+                        break;
+                }
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+            } while (input != ConsoleKey.Escape);
+        }
+        static void Evaluate()
+        {
+            
+            Console.WriteLine("Enter your RPN input");
+            string rpn = Console.ReadLine();
+            Stack myStack = new Stack(rpn.Length);
+            for (int i = 0; i < rpn.Length; i++)
+            {
+                if (rpn[i] == '/' || rpn[i] == '*' || rpn[i] == '+' || rpn[i] == '-')
+                {
+                    Operate((char)rpn[i], ref myStack);
+                }
+                else
+                {
+                    int a;
+                    int.TryParse(rpn[i].ToString(), out a);
+                    myStack.Push(a);
+                }
+            }
+            int b = myStack.Pop();
+            if (myStack.IsEmpty())
+            {
+                Console.WriteLine("Result: " + b);
+            }
+            else
+            {
+                Console.WriteLine("The stack is not empty and there are no operands left.");
+                Console.WriteLine("Current contents:");
+                List<int> contents = new List<int> { b };
+                while (!myStack.IsEmpty())
+                {
+                    contents.Add(myStack.Pop());
+                }
+                contents.Reverse();
+                for (int i = 0; i < contents.Count; i++)
+                {
+                    Console.WriteLine(contents[i]);
+                }
+            }
+        }
+        static void Operate(char operand, ref Stack myStack)
+        {
+            if (operand == '+')
+            {
+                int temp = myStack.Pop();
+                myStack.Push(myStack.Pop() + temp);
+            }
+            if (operand == '-')
+            {
+                int temp = myStack.Pop();
+                myStack.Push(myStack.Pop() - temp);
+            }
+            if (operand == '*')
+            {
+                int temp = myStack.Pop();
+                myStack.Push(myStack.Pop() * temp);
+            }
+            if (operand == '/')
+            {
+                int temp = myStack.Pop();
+                myStack.Push(myStack.Pop() / temp);
+            }
+        }
+    }
+}
---
+++ L155 Stack and RPN/Stack.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L155_Stack_and_RPN
+{
+    internal class Stack
+    {
+        private List<int> stack; private int maxsize;
+
+        public Stack(int initialSize = 0)
+        {
+            stack = new List<int>();
+            maxsize = initialSize;
+            //Random rand = new Random();
+            //for (int i = 0; i < maxsize; i++)
+            //{
+            //    stack.Add(rand.Next(0, 100));
+            //}
+        }
+        public int Count()
+        {
+            return stack.Count;
+        }
+
+        public bool Peek(ref int num)
+        {
+            if (!IsEmpty())
+            {
+                num = stack[stack.Count - 1];
+                return true;
+            }
+            else return false;
+        }
+        public int Peek()
+        {
+            if (!IsEmpty())
+            {
+                int num = stack[stack.Count - 1];
+                return num;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public bool Pop(ref int num)
+        {
+            if (!IsEmpty())
+            {
+                int temp = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                num = temp;
+                return true;
+            }
+            else return false;
+        }
+        public int Pop()
+        {
+            if (!IsEmpty())
+            {
+                int temp = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                return temp;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void Push(int num)
+        {
+            if (!IsFull())
+            {
+                stack.Add(num);
+                Console.WriteLine("Added " + num + " to the stack");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to add {num} to the stack");
+            }
+        }
+        public bool IsEmpty()
+        {
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Error: IsEmpty");
+                return true;
+            }
+            return false;
+        }
+        public bool IsFull()
+        {
+            if (stack.Count == maxsize)
+            {
+                Console.WriteLine("Error: IsFull");
+                return true;
+            }
+            return false;
+        }
+    }
+}
